6927c1022afabc053ecf974ead91bea7b1f2fdce
Devang Vishnu <thedevangvishnu@gmail.com>
2024-05-11 15:20:23 +0530

    Add dark-mode functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/mode-toggle";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
       <h1 className="text-4xl font-bold">StudyBuddy Landing Page</h1>
+      <ModeToggle />
     </main>
   );
 }
